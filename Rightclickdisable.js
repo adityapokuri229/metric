fe60ce29cd8d0fbe5e62065faec9d50d98c6807c
adityapokuri229 <81099814+adityapokuri229@users.noreply.github.com>
2021-05-17 14:45:07 +0530

    Update Rightclickdisable.js

--- Rightclickdisable.js
+++ Rightclickdisable.js
@@ -1,5 +1,5 @@
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
-  alert("Right click is disabled to protect Copyright laws 2020")
+  alert("Right click is disabled by Aditya to protect Copyright laws 2020")
 });
